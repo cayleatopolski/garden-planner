d613ae8c2c97299aee14af0a7e71ea0e8ebe29fb
Jeremy <xdetroit@live.com>
2019-09-09 16:15:52 -0400

    created side nav and nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,30 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { HttpClientModule } from "@angular/common/http";
 import { GardenService } from "./garden.service";
-import { PlantSearchComponent } from './plant-search/plant-search.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { GardenComponent } from './garden/garden.component';
+import { PlantSearchComponent } from "./plant-search/plant-search.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { GardenComponent } from "./garden/garden.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "../Material-Module";
 
 @NgModule({
-  declarations: [AppComponent, PlantSearchComponent, HomeComponent, AboutComponent, GardenComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    PlantSearchComponent,
+    HomeComponent,
+    AboutComponent,
+    GardenComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
   providers: [GardenService],
   bootstrap: [AppComponent]
 })
