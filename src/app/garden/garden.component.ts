4fd25407530e7d833990ce15b29de693796425a8
Carly Carnovale <carlycarnovale@gmail.com>
2019-09-09 16:16:58 -0400

    icons and updated about page

--- src/app/garden/garden.component.ts
+++ src/app/garden/garden.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GardenService } from '../garden.service';
+
 @Component({
   selector: 'app-garden',
   templateUrl: './garden.component.html',
