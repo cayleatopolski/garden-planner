a0a76ab95f5fe5ae78e92541729d3483f8ad48a5
Caitlin <caitlin@griffus.com>
2019-09-10 17:15:47 -0400

    search working

--- src/app/plant-search/plant-search.component.ts
+++ src/app/plant-search/plant-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { GardenService } from "../services/garden.service";
 import { NgForm } from "@angular/forms";
+import { flatMap } from "rxjs/operators";
 
 @Component({
   selector: "app-plant-search",
@@ -8,8 +9,7 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./plant-search.component.css"]
 })
 export class PlantSearchComponent implements OnInit {
-  plantData: any[];
-  images: any[] = [];
+  plantData: any[] = [];
   show: boolean = false;
 
   constructor(private gardenService: GardenService) {}
@@ -37,11 +37,13 @@ export class PlantSearchComponent implements OnInit {
   submitForm(form: NgForm) {
     this.gardenService
       .getPlantData(form.value.searchTerm)
-      .subscribe(response => {
-        this.plantData = response;
-        // console.log(this.plantData);
+      .pipe(
+        flatMap(shortPlantData => {
+          return this.gardenService.getId(shortPlantData);
+        })
+      )
+      .subscribe((plantData: any) => {
+        this.plantData = plantData;
       });
-
-    this.images = this.gardenService.getId(this.plantData);
   }
 }
