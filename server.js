77c111e2fd0b3bfcdf95e629eef3c9d4abb105b7
Carly Carnovale <carlycarnovale@gmail.com>
2019-09-11 14:48:11 -0400

    heroku setup

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-const port = 5000;
+// const port = 5000;
+const port = process.env.PORT || 5000;
 const router = require("./routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
