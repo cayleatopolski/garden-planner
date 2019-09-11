84db8724a8e22f7fe0dc3fa33306f63bbdafaaa5
Carly Carnovale <carlycarnovale@gmail.com>
2019-09-11 16:42:35 -0400

    changed it back

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + "/dist"));
 app.use("/", router);
+app.use(express.static(__dirname + "/dist"));
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
